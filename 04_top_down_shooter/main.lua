e8c8c603933741b0ef6e15a9b2160816fbb798ce
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2018-04-16 13:47:53 -0300

    Implement the topdown zombie shooting game.

--- 04_top_down_shooter/main.lua
+++ 04_top_down_shooter/main.lua
@@ -1,5 +1,85 @@
+local idealfps = 60
+
+-- Utility Functions
+
+function playerMouseAngle()
+    return angleTwoPoints(player.x, player.y, love.mouse.getX(), love.mouse.getY())
+end
+
+function zombiePlayerAngle(zombie)
+    return angleTwoPoints(zombie.x, zombie.y, player.x, player.y)
+end
+
+function angleTwoPoints(x1, y1, x2, y2)
+    return math.atan2(y2-y1,x2-x1)
+end
+
+function spawnZombie()
+    local zombie = {}
+    zombie.x = 0
+    zombie.y = 0
+    zombie.speed = 2.5*idealfps
+    zombie.a = 0
+    zombie.dead = false
+
+    -- Randomize zombie position from one of the sides of the screen
+    local side = math.random(1, 4)
+    if side == 1 then
+        -- left side of the screen
+        zombie.x = -30
+        zombie.y = math.random(0, love.graphics.getHeight())
+    elseif side == 2 then
+        -- right side of the screen
+        zombie.x = love.graphics.getWidth() + 30
+        zombie.y = math.random(0, love.graphics.getHeight())
+    elseif side == 3 then
+        -- bottom side of the screen
+        zombie.y = love.graphics.getHeight() + 30
+        zombie.x = math.random(0, love.graphics.getWidth())
+    else 
+        -- top side of the screen
+        zombie.y = -30
+        zombie.x = math.random(0, love.graphics.getWidth())
+    end  
+
+    table.insert( zombies, zombie )
+end
+
+function spawnBullet()
+    local bullet = {}
+    bullet.x = player.x
+    bullet.y = player.y
+    bullet.speed = 6*idealfps
+    bullet.a = player.a
+    bullet.dead = false
+
+    sound.shoot:stop()
+    sound.shoot:play()
+
+    table.insert( bullets, bullet)
+end
+
+function distanceBetween(x1, y1, x2, y2)
+    return math.sqrt((x1 - x2)^2, (y1 - y2)^2)
+end
+
+function clearTable(t)
+    for i,v in ipairs(t) do
+        t[i] = nil
+    end
+end
+
+-- Love functions
+
 function love.load()
-    local idealfps = 60
+    love.window.setTitle("Top-Down Shooter")
+
+    sound = {}
+    -- Static is good for small audio files to be kept into memory
+    sound.shoot = love.audio.newSource("sound/shoot.wav", "static")
+    -- Stream large audio files
+    sound.music = love.audio.newSource("sound/DangerStorm.mp3", "stream")
+    sound.music:play()
 
     sprites = {}
     sprites.player = love.graphics.newImage('sprites/player.png')
@@ -11,11 +91,14 @@ function love.load()
     player.x = 200
     player.y = 200
     player.speed = 3 * idealfps
+    player.a = 0
 
-    love.window.setTitle("Top-Down Shooter")
+    zombies = {}
+    bullets = {}
 end
 
 function love.update(dt)
+    -- Handle Player
     local speed = player.speed*dt
     if love.keyboard.isDown("s") then
         player.y = player.y + speed
@@ -29,9 +112,74 @@ function love.update(dt)
     if love.keyboard.isDown("d") then
         player.x = player.x + speed
     end
+    player.a = playerMouseAngle()
+
+    -- Handle bullets
+    for i,b in ipairs(bullets) do
+        b.x = b.x + math.cos(b.a) * b.speed * dt
+        b.y = b.y + math.sin(b.a) * b.speed * dt
+    end
+
+    -- Handle Zombies
+    for i,z in ipairs(zombies) do
+        z.a = zombiePlayerAngle(z)
+        z.x = z.x + math.cos(z.a) * z.speed * dt
+        z.y = z.y + math.sin(z.a) * z.speed * dt
+
+        -- Zombies x players
+        if distanceBetween(z.x, z.y, player.x, player.y) < 25 then
+            clearTable(zombies)
+        end
+
+        -- Zombies x bullets
+        for j,b in ipairs(bullets) do
+            if distanceBetween(z.x, z.y, b.x, b.y) < 20 then
+                z.dead = true
+                b.dead = true
+            end
+        end
+    end
+
+    -- Clean zombies
+    for i=#zombies,1,-1 do
+        if zombies[i].dead then
+            table.remove(zombies, i)
+        end
+    end
+
+    -- Clean Bullets
+    for i=#bullets,1,-1 do
+        local b = bullets[i]
+        if b.x < 0 or b.y < 0 or b.x > love.graphics.getWidth() or b.y > love.graphics.getHeight() or b.dead then
+            table.remove(bullets, i)
+        end
+    end
 end
 
 function love.draw()
     love.graphics.draw(sprites.background,0,0)
-    love.graphics.draw(sprites.player, player.x, player.y)
+    love.graphics.draw(sprites.player, player.x, player.y, player.a,
+                       nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
+
+    for i,z in ipairs(zombies) do
+        love.graphics.draw(sprites.zombie, z.x, z.y, z.a, nil, nil,
+                           sprites.zombie:getWidth()/2, sprites.zombie:getHeight()/2)
+    end
+
+    for i,b in ipairs(bullets) do
+        love.graphics.draw(sprites.bullet, b.x, b.y, nil, 0.5, 0.5,
+                           sprites.bullet:getWidth()/2, sprites.bullet:getHeight()/2)
+    end
+end
+
+function love.keypressed(key, scancode, isrepeat)
+    if key == "space" then
+        spawnZombie()
+    end
+end
+
+function love.mousepressed( x, y, b, istouch)
+    if b == 1 then
+        spawnBullet()
+    end
 end
